092b751c4667f785c00cf77ca58c28673c1a6fae
ancelmoth <ancelmoth@gmail.com>
2023-04-09 13:34:24 -0300

    Finalizado para testar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function gerarQRCode(){
     var inputUser = document.querySelector("textarea").value
   
-    var googleChart = "https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl="
+    var googleChart = "https://chart.googleapis.com/chart?cht=qr&chs=300x300&chld=H&chl="
     var conteudoQrCode = googleChart + encodeURIComponent(inputUser);
     document.querySelector("#QRCodeImage").src = conteudoQrCode
 
